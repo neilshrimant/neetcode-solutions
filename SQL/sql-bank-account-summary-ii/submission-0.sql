41092884081d6b53275e36a36376c35c06d6f8c0
neilshrimant <nilesh.shrimant@gmail.com>
2026-09-18 19:56:39 +0530

    Add: sql-bank-account-summary-ii - submission-0

---
+++ SQL/sql-bank-account-summary-ii/submission-0.sql
@@ -0,0 +1,11 @@
+-- Write your query below
+SELECT
+    u.name,
+    SUM(t.amount) AS balance
+FROM
+    users u LEFT JOIN transactions t
+        ON u.account = t.account
+GROUP BY
+    u.name
+HAVING
+    SUM(t.amount) > 10000
